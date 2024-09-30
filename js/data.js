ccabbb7553c4b8ac22979abfabd1ac0614e57b87
Dayana1803 <dayana.pereira.uy@gmail.com>
2024-09-30 18:48:45 -0300

    ahora si anda

---
+++ js/data.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const storedData = localStorage.getItem('data');
+    if (storedData) {
+        document.getElementById('data').textContent = storedData;
+    }
+});
+
